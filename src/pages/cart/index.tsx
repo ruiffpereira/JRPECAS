30b989ec1ce5dc3aa8aeff0fd04aab1ab22c9e9d
Rui Pereira <r_f@live.com.pt>
2025-03-27 23:38:50 +0000

    update cart and total ship 1

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -17,87 +17,89 @@ const Cart: React.FC = () => {
       {cart.products.length === 0 ? (
         <p className="text-white">Seu carrinho está vazio.</p>
       ) : (
-        <div>
+        <Fragment>
           <div className="text-end mb-4  text-lg">
             Total:
             <span className="ml-2 font-bold text-2xl">${cart.shipPrice}€</span>
           </div>
-          <table className="min-w-full bg-gray-800 text-white rounded-md">
-            <thead className="sticky bg-gray-800 z-10 top-[108px] lg:top-[72px]">
-              <tr>
-                <th className="py-2 px-2 border-b text-start border-gray-700">
-                  Imagem
-                </th>
-                <th className="py-2 px-2 border-b text-start border-gray-700">
-                  Nome
-                </th>
-                <th className="py-2 px-2 border-b text-start border-gray-700">
-                  Quantidade
-                </th>
-                <th className="py-4 px-2 border-b text-start border-gray-700">
-                  Preço
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {cart.products.map((item, index) => {
-                return (
-                  <tr key={index} className="hover:bg-gray-700">
-                    <td className="py-2 px-4 border-b border-gray-700">
-                      <div className="relative w-24 h-24">
-                        <Image
-                          src={`${URL_RAIZ}/${item.photos[0]}`}
-                          alt={item.name}
-                          width={200}
-                          height={200}
-                          style={{
-                            objectFit: 'contain',
-                            width: '100%',
-                            height: '100%',
-                          }}
-                          className="rounded-md"
-                        />
-                      </div>
-                    </td>
-                    <td className="py-2 px-4 border-b border-gray-700">
-                      {item.name}
-                    </td>
-                    <td className="py-2 px-4 border-b border-gray-700">
-                      <div className="flex gap-1 items-center">
-                        <button
-                          className="text-4xl"
-                          onClick={() =>
-                            addToCart({
-                              productId: item.productId,
-                              quantity: 0,
-                            })
-                          }
-                        >
-                          <CiSquareMinus />
-                        </button>
-                        <div className="w-7 text-center">{item.quantity}</div>
-                        <button
-                          className="text-4xl"
-                          onClick={() =>
-                            addToCart({
-                              productId: item.productId,
-                              quantity: 1,
-                            })
-                          }
-                        >
-                          <CiSquarePlus />
-                        </button>
-                      </div>
-                    </td>
-                    <td className="py-2 px-4 border-b border-gray-700">
-                      {item.price}€
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        </div>
+          <div className="overflow-auto">
+            <table className="min-w-full bg-gray-800 text-white rounded-md">
+              <thead className="bg-gray-800 z-10 ">
+                <tr>
+                  <th className="py-2 px-2 border-b text-start border-gray-700">
+                    Imagem
+                  </th>
+                  <th className="py-2 px-2 border-b text-start border-gray-700">
+                    Nome
+                  </th>
+                  <th className="py-2 px-2 border-b text-start border-gray-700">
+                    Quantidade
+                  </th>
+                  <th className="py-4 px-2 border-b text-start border-gray-700">
+                    Preço
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.products.map((item, index) => {
+                  return (
+                    <tr key={index} className="hover:bg-gray-700">
+                      <td className="py-2 px-4 border-b border-gray-700">
+                        <div className="relative w-24 h-24">
+                          <Image
+                            src={`${URL_RAIZ}/${item.photos[0]}`}
+                            alt={item.name}
+                            width={200}
+                            height={200}
+                            style={{
+                              objectFit: 'contain',
+                              width: '100%',
+                              height: '100%',
+                            }}
+                            className="rounded-md"
+                          />
+                        </div>
+                      </td>
+                      <td className="py-2 px-4 border-b border-gray-700">
+                        {item.name}
+                      </td>
+                      <td className="py-2 px-4 border-b border-gray-700">
+                        <div className="flex gap-1 items-center">
+                          <button
+                            className="text-4xl"
+                            onClick={() =>
+                              addToCart({
+                                productId: item.productId,
+                                quantity: 0,
+                              })
+                            }
+                          >
+                            <CiSquareMinus />
+                          </button>
+                          <div className="w-7 text-center">{item.quantity}</div>
+                          <button
+                            className="text-4xl"
+                            onClick={() =>
+                              addToCart({
+                                productId: item.productId,
+                                quantity: 1,
+                              })
+                            }
+                          >
+                            <CiSquarePlus />
+                          </button>
+                        </div>
+                      </td>
+                      <td className="py-2 px-4 border-b border-gray-700">
+                        {item.price}€
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </Fragment>
       )}
       <div className="mt-8">
         <Link href="/" className="text-red-500 hover:underline">
